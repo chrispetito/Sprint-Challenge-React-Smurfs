17a741e6a9320d3a5cacd9a68b924ae46d189e05
chrispetito <christopherpetito@gmail.com>
2019-05-10 09:15:55 -0700

    add post request, routes, links for home and form

--- village/src/App.js
+++ village/src/App.js
@@ -1,41 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
-import axios from 'axios';
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
+import axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
 
   componentDidMount() {
     axios
-    .get(`http://localhost:3333/smurfs`)
-    .then(res => {
-      console.log(res)
-      this.setState({
-        smurfs: res.data
+      .get(`http://localhost:3333/smurfs`)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          smurfs: res.data
+        });
       })
-    })
-    .catch(err => {
-      console.log(err);
-    })
+      .catch(err => {
+        console.log(err);
+      });
   }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
 
-
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        {/* <Smurfs smurfs={this.state.smurfs} /> */}
+        <nav>
+          <Link to='/'>Home</Link>
+          <Link to='/smurf-form'>Add Smurf</Link>
+        </nav>
+        <Route
+          exact
+          path="/"
+          render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
+        />
+        <Route path="/smurf-form" component={SmurfForm} />
       </div>
     );
   }
